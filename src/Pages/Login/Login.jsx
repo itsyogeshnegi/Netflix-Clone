c92840285e7cd8cf869244f0f285b75ae696c810
yogesh <yogesh@locad.net>
2023-03-13 11:26:02 +0530

    log-in-out

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
-
+import { toast, ToastContainer } from "react-toastify";
+import { Link, useNavigate } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css';
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    //getting email password
+    const userName = localStorage.getItem("email")
+        ? localStorage.getItem("email")
+        : "admin@admin.com";
+    const userPassword = localStorage.getItem("password")
+        ? localStorage.getItem("password")
+        : "admin";
+
+    //submit function
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (email === userName && password === userPassword) {
+            toast.success("Login Success");
+            navigate("/");
+        } else {
+            toast.error("Invalid Email OR password");
+        }
+    };
+
     return (
         <div className='Login'>
             <h1>Log-In</h1>
             <form>
-                <input type="email" placeholder='Email' />
-                <input type="password" placeholder='Password' />
-                <button type="submit">SUBMIT</button>
+                <input type="email" placeholder='Email' value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+
+                <input type="password" placeholder='Password' value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+
+                <button type="submit" onClick={handleSubmit}>SUBMIT</button>
             </form>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
         </div>
     )
 }
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,16 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 const Signup = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const sendData = (e) => {
+        e.preventDefault()
+        if (name === "") {
+            alert("Name is required")
+        }
+        else if (email === "") {
+            alert("Email is required")
+        }
+        else if (password === "") {
+            alert("password is required")
+        }
+        else {
+            localStorage.setItem("name", name);
+            localStorage.setItem("email", email);
+            localStorage.setItem("password", password);
+            toast.success("submit is Done")
+        }
+    }
     return (
-        <div className='signup'>
+        <div className='signup' >
             <h1>Sign-Up</h1>
             <form>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder='Email' />
-                <input type="password" placeholder='Password' />
-                <button type="submit">SUBMIT</button>
+                <input
+                    name='name'
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <input
+                    name='email'
+                    type="email"
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+                    name='password'
+                    type="password"
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button type="submit" onClick={sendData}>SUBMIT</button>
             </form>
-        </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
+        </div >
     )
 }
 
