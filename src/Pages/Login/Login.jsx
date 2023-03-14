791354c269be915056c8502f6ecf7477ad2471e5
yogesh <yogesh@locad.net>
2023-03-14 20:55:17 +0530

    Auth

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,36 +2,50 @@ import React, { useState } from 'react'
 import './Login.css'
 import { toast, ToastContainer } from "react-toastify";
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import StepOne from '../Choose-Plans/StepOne';
 import 'react-toastify/dist/ReactToastify.css';
+import { get, getDatabase, ref, child, Database } from 'firebase/database'
+import { app } from '../../Firebaseconnection'
+
+
 const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    const auth = getAuth(app);
+
     //getting email password
-    const userName = localStorage.getItem("email")
-        ? localStorage.getItem("email")
-        : "admin@admin.com";
-    const userPassword = localStorage.getItem("password")
-        ? localStorage.getItem("password")
-        : "admin";
+    // const userName = localStorage.getItem("email")
+    //     ? localStorage.getItem("email")
+    //     : "admin@admin.com";
+    // const userPassword = localStorage.getItem("password")
+    //     ? localStorage.getItem("password")
+    //     : "admin";
 
     //submit function
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (email === userName && password === userPassword) {
-            toast.success("Login Success");
-            navigate("/stepone");
-        } else {
-            toast.error("Invalid Email OR password");
+
+        if (email === "") {
+            toast.warning("Fill your Email")
+        }
+        else if (password === "") {
+            toast.warning("Fill your Password")
+        }
+        else {
+            signInWithEmailAndPassword(auth, email, password)
+                .then((value) => navigate("/stepone"))
+                .catch((err) => toast.error("Invaild Id or Password"))
         }
     };
 
     return (
         <div className='Login'>
-            <h1>Log-In</h1>
             <form>
+                <h1>Log In</h1>
+
                 <input type="email" placeholder='Email' value={email}
                     onChange={(e) => setEmail(e.target.value)} />
 
@@ -39,10 +53,11 @@ const Login = () => {
                     onChange={(e) => setPassword(e.target.value)} />
 
                 <button type="submit" onClick={handleSubmit}>SUBMIT</button>
+                <p>New to Netflix?<Link to="/signup" style={{ color: "white" }}>Sign up now</Link></p>
             </form>
             <ToastContainer
-                position="top-center"
-                autoClose={5000}
+                position="top-right"
+                autoClose={1000}
                 hideProgressBar={false}
                 newestOnTop={false}
                 closeOnClick
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Signup.css'
-import { getDatabase, ref, set, onValue, DataSnapshot, push } from "firebase/database"
+import { getDatabase } from "firebase/database"
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 import { app } from '../../Firebaseconnection'
 import { toast, ToastContainer } from "react-toastify";
@@ -11,76 +11,54 @@ const Signup = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const db = getDatabase(app);
+    // const db = getDatabase(app);
+    const auth = getAuth(app);
     const navigate = useNavigate();
-
-    // const sendData = (e) => {
-    //     e.preventDefault()
-    //     if (name === "") {
-    //         alert("Name is required")
-    //     }
-    //     else if (email === "") {
-    //         alert("Email is required")
-    //     }
-    //     else if (password === "") {
-    //         alert("password is required")
-    //     }
-    //     else {
-    //         localStorage.setItem("name", name);
-    //         localStorage.setItem("email", email);
-    //         localStorage.setItem("password", password);
-    //         toast.success("submit is Done")
-    //     }
-    // }
 
     const sendData = async (e) => {
 
         e.preventDefault();
         var atposition = email.indexOf("@");
-        var dotposition = email.lastIndexOf(".")
+        var dotposition = email.lastIndexOf(".");
 
-            ; if (name === "") {
-                toast.error("Name is required")
-            }
+        if (name === "") {
+            toast.error("Name is required")
+        }
         else if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= email.length || email === "") {
             toast.error("Email is required")
         }
         else if (password === "") {
             toast.error("password is required")
         }
+        else if (password.length < 6) {
+            toast.warning("Your password minimum 6 digit")
+        }
         else {
-            toast.success(
-
-                // db.push("root/users", { name, email, password })
-
-                push(ref(db, "users/"), {
-                    name,
-                    email,
-                    password
-                })
-            )
+            toast.success("Create Done",
+                createUserWithEmailAndPassword(auth, email, password).then((value) => console.log("create Done")))
             navigate("/");
         };
     }
 
     return (
         <div className='signup' >
-            <h1>Sign-Up</h1>
             <form>
+                <h1>Sign Up</h1>
+
                 <input
                     name='name'
                     type="text"
                     placeholder="Name"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
-                    required
+                // required
                 />
 
                 <input
                     name='email'
                     type="email"
                     placeholder='Email'
-                    required
+                    // required
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
@@ -90,15 +68,15 @@ const Signup = () => {
                     type="password"
                     placeholder='Password'
                     value={password}
-                    required
+                    // required
                     onChange={(e) => setPassword(e.target.value)}
                 />
 
                 <button type="submit" onClick={sendData}>SUBMIT</button>
             </form>
             <ToastContainer
-                position="top-center"
-                autoClose={3000}
+                position="top-right"
+                autoClose={1000}
                 hideProgressBar={false}
                 newestOnTop={false}
                 closeOnClick
